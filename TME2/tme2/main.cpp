d0cdcf6818ce57ad0dd36ae15db93bd53d160096
Wilfrid LYAUTEY <wilfrid.lyautey@etu.sorbonne-universite.fr>
2024-09-26 10:46:33 +0200

    sort + tri

--- TME2/tme2/main.cpp
+++ TME2/tme2/main.cpp
@@ -2,19 +2,25 @@
 #include <fstream>
 #include <regex>
 #include <chrono>
+#include <vector>
+
 
 int main () {
 	using namespace std;
 	using namespace std::chrono;
 
-	ifstream input = ifstream("/tmp/WarAndPeace.txt");
+	ifstream input = ifstream("WarAndPeace.txt");
 
 	auto start = steady_clock::now();
 	cout << "Parsing War and Peace" << endl;
-
+	
 	size_t nombre_lu = 0;
 	// prochain mot lu
 	string word;
+
+	std::vector<std::string> vect;
+	int count = 0;
+
 	// une regex qui reconnait les caractères anormaux (négation des lettres)
 	regex re( R"([^a-zA-Z])");
 	while (input >> word) {
@@ -28,8 +34,26 @@ int main () {
 			// on affiche un mot "propre" sur 100
 			cout << nombre_lu << ": "<< word << endl;
 		nombre_lu++;
+
+		vect.push_back(word); //Vecteur 1 avec tous les mots
+
+
+		// for(std::string i:vect){
+		// 	if(i == word){
+		// 		break;
+		// 	}
+		// 	vect.push_back(word);
+		// }
 	}
 	input.close();
+
+	sort(vect.begin(),vect.end());
+
+	for(auto i = 1; i < vect.size();i++){
+		if(vect[i] != vect[i -1]){
+			count ++;
+		}
+	}
 
 	cout << "Finished Parsing War and Peace" << endl;
 
@@ -39,7 +63,7 @@ int main () {
               << "ms.\n";
 
     cout << "Found a total of " << nombre_lu << " words." << endl;
-
+	cout << "Vecteur size : " << count << endl;
     return 0;
 }
 
