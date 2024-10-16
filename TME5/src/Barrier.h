533bd5e5f5c8361ff9267522bd687babf4826916
Wilfrid LYAUTEY <wilfrid.lyautey@etu.sorbonne-universite.fr>
2024-10-16 20:27:16 +0200

    Question 8 en suspens

---
+++ TME5/src/Barrier.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <mutex>
+#include <condition_variable>
+
+class Barrier{
+    int counter;
+    const int N;
+    std::mutex m;
+    std::condition_variable cv;
+public:
+    Barrier(int N):counter(0),N(N){}
+    
+    void done(){
+        std::unique_lock<std::mutex> lg(m);
+        counter++;
+        if(counter == N){
+            cv.notify_all();
+        }
+    }
+    
+    void waitor(){
+        std::unique_lock<std::mutex> lg(m);
+        while(counter != N){
+            cv.wait(lg);
+        }
+    }
+
+};
+
