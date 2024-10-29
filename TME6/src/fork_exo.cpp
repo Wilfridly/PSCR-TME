ad5b8533a03286c3c61d4e8c320551caf40166bb
Wilfridly <wilfrid.lyautey@etu.sorbonne-universite.fr>
2024-10-29 23:04:50 +0100

    maj TME6

--- TME6/src/fork_exo.cpp
+++ TME6/src/fork_exo.cpp
@@ -1,18 +1,45 @@
+#include <sys/wait.h>
 #include <iostream>
 #include <unistd.h>
 
+
 int main () {
 	const int N = 3;
-	std::cout << "main pid=" << getpid() << std::endl;
+	int cpt = 0;
+	int pid;
+	int mainpid = getpid();
+	std::cout << "main pid =" << mainpid << std::endl;
 
-	for (int i=1, j=N; i<=N && j==N && fork()==0 ; i++ ) {
-		std::cout << " i:j " << i << ":" << j << std::endl;
+	for (int i=1, j=N; i<=N && j==N; i++ ) {
+		if(fork() == 0){
+			cpt = 0;
+		}else{
+			std::cout << " i:j " << i << ":" << j << std::endl;
+			cpt++;
+			break;
+		}
+
 		for (int k=1; k<=i && j==N ; k++) {
 			if ( fork() == 0) {
+				cpt= 0;
 				j=0;
 				std::cout << " k:j " << k << ":" << j << std::endl;
+			}else{
+				cpt++;
 			}
 		}
 	}
+	
+	
+	for (int i=0; i < cpt; i++) {
+		// wait(nullptr);
+		int status;
+		int pid = wait(&status);
+	}
+	std::cout << "Fin du processus " << getpid() << std::endl;
+	if(mainpid == getpid()){
+		std::cout << "ouuiiii le père attend ses fils" << std::endl;
+	}
+	
 	return 0;
 }
---
+++ TME6/src/main.cpp
@@ -0,0 +1,38 @@
+#include <sys/wait.h>
+#include <iostream>
+#include <unistd.h>
+#include <csignal>
+#include "rsleep.h"
+
+void attaque (pid_t adversaire){
+    sigset_t mask;
+    sigfillset(&mask);
+    sigdelset(&mask, SIGINT);
+
+    signal(SIGINT,[](int ){sig--;});
+
+}
+
+void defense (){
+    //
+    randsleep()
+}
+
+void combat (pid_t adversaire){
+    while(true){
+        defense();
+        
+        attaque(adversaire);
+    }
+}
+
+
+int main(){ //
+    int life = 3;
+
+    if(fork() == 0){ //Luke
+
+    }else{ // Vador
+
+    }
+}
